14d05f125d0c59996debab29726e11789a055b91
Ifthe16 <habiburrahmanif975@gmail.com>
2024-10-27 17:58:08 +0600

    added components and bottles json file and loaded data in ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Bottles from './components/Bottles/Bottles'
+import Header from './components/Header/Header'
 
 function App() {
   return (
     <>
-      <h1>Vite + React</h1>
+      <Header></Header>
+      <Bottles></Bottles>
     </>
   )
 }
